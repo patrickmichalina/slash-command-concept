8d7cf94c27c4c4f0ac8e187fea7ed19f3ad4da76
Patrick Michalina <patrickmichalina@mac.com>
2022-05-03 01:31:33 -0500

    updates

--- apps/slash-command-concept/src/app/app.component.ts
+++ apps/slash-command-concept/src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { combineLatest, debounceTime, map, mergeMap, Observable, of, share } from "rxjs";
+import { combineLatest, debounceTime, map, mergeMap, Observable, of, share, startWith } from "rxjs";
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { FormControl } from "@angular/forms";
 
 interface Plugin {
-  fn: (value?: string) => void
+  fn: (...value: string[]) => unknown
   hint?: string
   input?: string
 }
@@ -24,18 +24,18 @@ export class AppComponent {
 
   // future extendable: could be configured from settings and async.
   public readonly registeredPlugins$: Observable<Record<string, Plugin>> = of({
-    giphy: { hint: '[search]', description: 'TODO', fn: () => alert('giphy not implemented') },
-    add: { hint: '[num] [num]', description: 'TODO', fn: (val?: string) => alert(val + 'add not implemented') },
-    sub: { hint: '[num] [num]', description: 'TODO', fn: () => alert('subtract not implemented') },
-    div: { hint: '[num] [num]', description: 'TODO', fn: () => alert('divide not implemented') },
-    mul: { hint: '[num] [num]', description: 'TODO', fn: () => alert('multiply not implemented') },
+    giphy: { hint: '[search]', description: 'TODO', fn: () => 'please setup giphy' },
+    add: { hint: '[num] [num]', description: 'TODO', fn: (...val: string[]) => val.map(b => parseInt(b)).filter(b => !isNaN(b)).reduce((acc, curr) => acc ? acc + curr : curr, 0) },
+    sub: { hint: '[num] [num]', description: 'TODO', fn: (...val: string[]) => val.map(b => parseInt(b)).filter(b => !isNaN(b)).reduce((acc, curr) => acc ? acc - curr : curr, 0) },
+    div: { hint: '[num] [num]', description: 'TODO', fn: (...val: string[]) => val.map(b => parseInt(b)).filter(b => !isNaN(b)).reduce((acc, curr) => acc ? acc / curr : curr, 0) },
+    mul: { hint: '[num] [num]', description: 'TODO', fn: (...val: string[]) => val.map(b => parseInt(b)).filter(b => !isNaN(b)).reduce((acc, curr) => acc ? acc * curr : curr, 0) },
     addcommand: { description: '', fn: () => alert('addcommand not implemented') }
   }).pipe(share())
 
   private readonly triggerStrings$ = combineLatest([this.registeredPlugins$, this.triggerKey$]).pipe(
     map(([plugins, triggerKey]) => [triggerKey, ...Object.keys(plugins).map(key => `${triggerKey}${key} `)])
   )
-  private readonly inputValueChanges$: Observable<string> = this.input.valueChanges.pipe(share())
+  private readonly inputValueChanges$: Observable<string> = this.input.valueChanges.pipe(startWith(''), share())
 
   public readonly shownPlugins$ = this.inputValueChanges$.pipe(
     debounceTime(0),
@@ -44,19 +44,27 @@ export class AppComponent {
         ? []
         : txt === b[0]
           ? b.slice(1)
-          : b.filter(c => txt[0] === c[0] && c.toLowerCase().includes(txt.toLowerCase()))),
+          : b.filter(c => txt[0] === c[0] && (
+            c.toLowerCase().includes(txt.toLowerCase()) ||
+            (txt.toLowerCase().includes(c.toLowerCase()))
+          )).slice(1)
+      ),
     )),
     mergeMap(keys => this.registeredPlugins$.pipe(
       map(pluginsObj => keys
         .map(a => a.trim())
-        .map(name => ({ ...pluginsObj[name.slice(1)], name, inputValue: this.input.value.slice(this.input.value.indexOf(" ")) })))
+        .map(name => ({ ...pluginsObj[name.slice(1)], name, inputValue: this.input.value.slice(this.input.value.indexOf(" ")).split(" ") })))
     ))
   )
 
-  submit(fc: FormControl): void {
-    const value = fc.value
+  submit(fc: FormControl, plugin: Plugin): void {
+    const value = fc.value as string
+
     if (!value || value === '' || value === null) return
-    this.chat.setValue(this.chat.value + "\n" + "user1: " + value)
+
+    const valueToSet = plugin ? plugin.fn(...value.split(" ").slice(1)) : value
+
+    this.chat.setValue(this.chat.value + "\n" + "user1: " + valueToSet)
     fc.setValue('')
   }
 
